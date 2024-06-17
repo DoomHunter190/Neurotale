73b392072cd15dd2b29adeeca7d7f2cee9527adf
Максим Портнов <maximportnov99@yandex.ru>
2024-06-17 23:10:34 +0300

    add func city, weather

--- main.py
+++ main.py
@@ -1,66 +1,61 @@
-import datetime, timedelta
+from datetime import datetime, timedelta
 import requests
 
 def get_city_location(city):
-    api_key = 'YOUR_GOOGLE_API_KEY'
-    base_url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    api_key = 'ec4b6970-5c79-4ff8-a566-f8e1d899c61f'
+    base_url = 'https://geocode-maps.yandex.ru/1.x/'
     params = {
-        'address': city,
-        'key': api_key
+        'geocode': city,
+        'apikey': api_key,
+        'format': 'json',
     }
 
     response = requests.get(base_url, params=params)
     data = response.json()
-    if data['status'] == 'OK':
-        location = data['results'][0]['geometry']['location']
-        latitude = location['lat']
-        longitude = location['lng']
-        print(f'Геолокация города {city}: Широта - {latitude}, Долгота - {longitude}')
+
+    if response.status_code == 200:
+        try:
+            coords = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+            longitude, latitude = map(float, coords.split())
+            print(f'Координаты города {city}: Широта - {latitude}, Долгота - {longitude}')
+            return latitude, longitude
+        except (KeyError, IndexError, ValueError):
+            print('Город не найден или данные не доступны')
     else:
-        print('Геолокация не найдена')
-    return latitude, longitude
+        print('Ошибка при получении координат')
+
 
 def get_weather(latitude, longitude):
-    api_key = 'YOUR_OPENWEATHERMAP_API_KEY'
-    base_url = 'https://api.openweathermap.org/data/2.5/onecall'
+    api_key = '46f1cbbca65cad26389133ee943cd628'
+    base_url = 'https://api.openweathermap.org/data/2.5/forecast'
 
-    # Получаем текущую дату
-    today = datetime.now()
-
-    # Вычисляем дату на следующий день
-    tomorrow = today + timedelta(days=1)
-
+    # Формируем параметры запроса
     params = {
         'lat': latitude,
         'lon': longitude,
         'appid': api_key,
-        'exclude': 'current,minutely,hourly',  # Исключаем текущую, минутную и часовую погоду
-        'units': 'metric'  # Возможные значения: 'metric', 'imperial', 'standard'
+        'cnt': 2,
+        'units': 'metric',
+        'lang': 'ru',
     }
 
+    # Делаем запрос к API
     response = requests.get(base_url, params=params)
     data = response.json()
-    
-    if response.status_code == 200:
-        forecast = data['daily'][1]  # Прогноз на завтра
-        weather = forecast['weather'][0]['description']
-        
-        print(f'Прогноз погоды на завтра:')
-        print(f'Погода: {weather}')
-    else:
-        print('Ошибка при получении прогноза погоды')
-    return weather
+    # Получаем информацию о погоде на завтра
+    temperature = data['list'][1]['main']['temp']
+    weather_description = data['list'][1]['weather'][0]['description']
+    return temperature, weather_description
 
 
 def create_prompt(city, genre, length, weather):
-    prompt = (f'Напишите рассказ в жанре: {genre}, о погоде: {weather} в городе: {city} завтра.
-              История не должна превышать {length} символов.')
+    prompt = (f'Напишите рассказ в жанре: {genre}, о погоде: {weather} в городе: {city}. История не должна превышать {length} символов.')
     return prompt
 
 
 def get_request_to_model(model_name, promt):
-    start_time = datetime.datetime.now()
-    end_time = datetime.datetime.now()
+    start_time = datetime.now()
+    end_time = datetime.now()
     duration = end_time - start_time
     return duration.total_seconds()
 
@@ -78,24 +73,25 @@ def input_genre():
 
 def main():
     city = input('Введите город: ')
-    genre = input_genre()
-    length = int(input('Введите длину рассказа в символах: '))
-    location = get_city_location(city)
-    longitude = location['latitude']
-    latitude = location['longitude']
-    weather = get_weather()
+    #genre = input_genre()
+    #length = int(input('Введите длину рассказа в символах: '))
+    latitude, longitude = get_city_location(city)
+    print(latitude)
+    print(longitude)
+    weather = get_weather(latitude, longitude)
+    print('Погода: ', weather)
 
-    prompt = create_prompt(city, genre, length, weather)
-    yandex_gpt_duration = get_request_to_model("YandexGPT", prompt)
-    gigachat_duration = get_request_to_model("GigaChat", prompt)
-    with open("yandexgpt_response.txt", "w") as file:
-        file.write("YandexGPT response")
+    #prompt = create_prompt(city, genre, length, weather)
+    #yandex_gpt_duration = get_request_to_model("YandexGPT", prompt)
+    #gigachat_duration = get_request_to_model("GigaChat", prompt)
+    #with open("yandexgpt_response.txt", "w") as file:
+    #    file.write("YandexGPT response")
 
-    with open("gigachat_response.txt", "w") as file:
-        file.write("GigaChat response")
+    #with open("gigachat_response.txt", "w") as file:
+    #    file.write("GigaChat response")
     
-    print("YandexGPT:", yandex_gpt_duration, "seconds", "yandexgpt_response.txt")
-    print("GigaChat:", gigachat_duration, "seconds", "gigachat_response.txt")
+    #print("YandexGPT:", yandex_gpt_duration, "seconds", "yandexgpt_response.txt")
+    #print("GigaChat:", gigachat_duration, "seconds", "gigachat_response.txt")
 
 if __name__ == '__main__':
     main()
